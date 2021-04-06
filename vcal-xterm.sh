f3565b55242b2b698de49397a9b25f85db7683aa
John D. Robertson <john@rrci.com>
2021-04-06 11:53:52 -0400

    Minor edits

--- vcal-xterm.sh
+++ vcal-xterm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# This script launches the output from vcal in the 'less' pager in a terminal window.
+# This script launches the output from vcalendar in the 'less' pager in a terminal window.
 
-if [[ $1 == STOP ]]; then
-   exec 2>&1 vcalendar "$2" | less
-fi
+[[ $1 == STOP ]] && exec 2>&1 vcalendar "$2" | less
 
 exec xfce4-terminal --geometry 95x40 -H -x "$0" STOP "$1"
